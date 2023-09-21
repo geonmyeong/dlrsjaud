889a7f0c2e86a36c5ed7280fd89e2f5717f317ef
geonmyeong <dldl26607@gmail.com>
2023-09-21 12:46:24 +0900

    "Merge 기초 구현"

---
+++ My project/Assets/Scripts/ItemInfo.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemInfo : MonoBehaviour
+{
+    public int slotId;      //½½·Ô ¹øÈ£
+    public int itemId;      //¾ÆÀÌÅÛ ¹øÈ£
+
+    public void InitDummy(int slotId, int itemId)
+    {
+        this.slotId = slotId;
+        this.itemId = itemId;
+    }
+}
